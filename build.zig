340f5435d173fec9290117452b3e3aaab3346848
Ian McFarlane <i.mcfarlane2002@gmail.com>
2023-09-04 19:45:53 -0400

    migrate to build.zig.zon for dependencies

--- build.zig
+++ build.zig
@@ -1,25 +1,33 @@
 const std = @import("std");
 const builtin = @import("builtin");
-const app_name = "abmoog";
+const app_name = "example_c_game";
 
-const release_flags = [_][]const u8{"-DNDEBUG"};
-const debug_flags = [_][]const u8{};
+const release_flags = [_][]const u8{ "-std=c11", "-DNDEBUG", "-DRELEASE" };
+const debug_flags = [_][]const u8{"-std=c11"};
+
 var chosen_flags: ?[]const []const u8 = null;
 
-const common = @import("./build/common.zig");
-const include = common.include;
-const link = common.link;
-const linkFlag = common.linkFlag;
-const includeFlag = common.includeFlag;
-const linkPrefixFlag = common.linkPrefixFlag;
-const includePrefixFlag = common.includePrefixFlag;
-const optionalPrefixToLibrary = common.optionalPrefixToLibrary;
-
-const cdb = @import("./build/compile_commands.zig");
-const makeCdb = cdb.makeCdb;
+const zcc = @import("compile_commands");
 
 const c_sources = [_][]const u8{
     "src/main.c",
+};
+
+const Library = struct {
+    // name in build.zig
+    remote_name: []const u8,
+    // the name given to this library in its build.zig. usually in addStaticLibrary
+    artifact_name: []const u8,
+    imported: ?*std.Build.Dependency,
+
+    fn artifact(self: @This()) *std.Build.CompileStep {
+        return self.imported.?.artifact(self.artifact_name);
+    }
+};
+
+var libraries = [_]Library{
+    .{ .remote_name = "raylib", .artifact_name = "raylib", .imported = null },
+    .{ .remote_name = "chipmunk2d", .artifact_name = "chipmunk", .imported = null },
 };
 
 pub fn build(b: *std.Build) !void {
@@ -29,21 +37,17 @@ pub fn build(b: *std.Build) !void {
     // keep track of any targets we create
     var targets = std.ArrayList(*std.Build.CompileStep).init(b.allocator);
 
+    for (libraries, 0..) |library, index| {
+        libraries[index].imported = b.dependency(library.remote_name, .{
+            .target = target,
+            .optimize = mode,
+        });
+    }
+
     // create executable
     var exe: ?*std.Build.CompileStep = null;
     // emscripten library
     var lib: ?*std.Build.CompileStep = null;
-
-    const libraries = try common.getLibraries(b);
-    var library_compile_steps = std.ArrayList(*std.Build.CompileStep).init(b.allocator);
-    for (libraries) |library| {
-        if (library.buildFn) |fun| {
-            const compilestep = try fun(b, target, mode);
-            try library_compile_steps.append(compilestep);
-            // get compile_commands.json for the lib
-            try targets.append(compilestep);
-        }
-    }
 
     switch (target.getOsTag()) {
         .wasi, .emscripten => {
@@ -65,18 +69,10 @@ pub fn build(b: *std.Build) !void {
 
             const emscripten_include_flag = try includePrefixFlag(b.allocator, b.sysroot.?);
 
-            var flags = std.ArrayList([]const u8).init(b.allocator);
-            try flags.append(emscripten_include_flag);
-            for (libraries) |library| {
-                for (library.allFlags()) |flag| {
-                    try flags.append(flag);
-                }
-            }
-
-            lib.?.addCSourceFiles(&c_sources, try flags.toOwnedSlice());
+            lib.?.addCSourceFiles(&c_sources, &[_][]const u8{emscripten_include_flag});
             lib.?.defineCMacro("__EMSCRIPTEN__", null);
             lib.?.defineCMacro("PLATFORM_WEB", null);
-            lib.?.addIncludePath(emscriptenSrc);
+            lib.?.addIncludePath(.{ .path = emscriptenSrc });
 
             const lib_output_include_flag = try includePrefixFlag(b.allocator, b.install_prefix);
             const shell_file = try std.fs.path.join(b.allocator, &.{ emscriptenSrc, "minshell.html" });
@@ -122,17 +118,11 @@ pub fn build(b: *std.Build) !void {
                 "-sEXPORTED_RUNTIME_METHODS=ccall,cwrap",
             };
 
-            var fullcommand = std.ArrayList([]const u8).init(b.allocator);
-            try fullcommand.appendSlice(command);
+            const emcc = b.addSystemCommand(command);
+
+            // also statically link the remote libraries
             for (libraries) |library| {
-                try fullcommand.appendSlice(&library.allFlags());
-            }
-
-            const emcc = b.addSystemCommand(try fullcommand.toOwnedSlice());
-
-            // also statically link the git libraries
-            for (library_compile_steps.items) |lib_cstep| {
-                emcc.addArtifactArg(lib_cstep);
+                emcc.addArtifactArg(library.artifact());
             }
             emcc.addArtifactArg(lib.?);
             emcc.step.dependOn(&lib.?.step);
@@ -165,13 +155,7 @@ pub fn build(b: *std.Build) !void {
 
             chosen_flags = if (mode == .Debug) &debug_flags else &release_flags;
 
-            var flags = std.ArrayList([]const u8).init(b.allocator);
-            try flags.appendSlice(chosen_flags.?);
-            for (libraries) |library| {
-                try flags.appendSlice(&library.allFlags());
-            }
-
-            exe.?.addCSourceFiles(&c_sources, try flags.toOwnedSlice());
+            exe.?.addCSourceFiles(&c_sources, chosen_flags.?);
 
             // always link libc
             for (targets.items) |t| {
@@ -205,10 +189,9 @@ pub fn build(b: *std.Build) !void {
 
     // make the targets depend on the lib compile steps
     for (&[_]?*std.Build.CompileStep{ exe, lib }) |mainstep| {
-        if (mainstep) |cstep| {
-            for (library_compile_steps.items) |lib_cstep| {
-                cstep.step.dependOn(&lib_cstep.step);
-                cstep.linkLibrary(lib_cstep);
+        if (mainstep) |step| {
+            for (libraries) |library| {
+                step.linkLibrary(library.artifact());
             }
         }
     }
@@ -217,17 +200,27 @@ pub fn build(b: *std.Build) !void {
         b.installArtifact(t);
     }
 
-    // compile commands step
-    cdb.registerCompileSteps(targets);
+    zcc.createStep(b, "cdb", try targets.toOwnedSlice());
+}
 
-    var step = try b.allocator.create(std.Build.Step);
-    step.* = std.Build.Step.init(.{
-        .id = .custom,
-        .name = "cdb_file",
-        .makeFn = makeCdb,
-        .owner = b,
-    });
+fn includePrefixFlag(ally: std.mem.Allocator, path: []const u8) ![]const u8 {
+    return try std.fmt.allocPrint(ally, "-I{s}/include", .{path});
+}
 
-    const cdb_step = b.step("cdb", "Create compile_commands.json");
-    cdb_step.dependOn(step);
+fn include(
+    targets: std.ArrayList(*std.Build.CompileStep),
+    path: []const u8,
+) !void {
+    for (targets.items) |target| {
+        target.addIncludePath(.{ .path = path });
+    }
+}
+
+fn link(
+    targets: std.ArrayList(*std.Build.CompileStep),
+    lib: []const u8,
+) !void {
+    for (targets.items) |target| {
+        target.linkSystemLibrary(lib);
+    }
 }
